5db1d38822aaa6c14de75b2cf5e1d2046c36f36d
Alain Guillon <a.guillon@it-students.fr>
2023-02-25 11:26:30 +0100

    first app 100% ok

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Dépendance
+import { useState } from 'react';
+import { StyleSheet, View, Image, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [bulbState, setBulbState] = useState(true);
+
+	return (
+		<View style={styles.container}>
+			{bulbState ? (
+				<Image
+					source={require('./assets/images/bulb_on.png')}
+					style={{ width: '100%', height: '90%' }}
+				/>
+			) : (
+				<Image
+					source={require('./assets/images/bulb_off.png')}
+					style={{ width: '100%', height: '90%' }}
+				/>
+			)}
+
+			<Button
+				title={bulbState ? 'Éteindre' : 'Allumer'}
+				color='#122a2e'
+				onPress={() => setBulbState(!bulbState)}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'black',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
